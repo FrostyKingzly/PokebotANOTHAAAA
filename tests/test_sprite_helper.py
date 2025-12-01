f7bebc03749278fcdee0dc27931c0dbe5a674cf8
FrostyKingzly <xdshooter5stream@gmail.com>
2025-11-30 22:37:43 -0500

    Prioritize static Gen5 sprites when animations are missing

--- tests/test_sprite_helper.py
+++ tests/test_sprite_helper.py
@@ -13,14 +13,38 @@ def test_showdown_slug_normalization_strips_accents():
     assert url.endswith("/flabebe.png")
 
 
-def test_modern_species_default_to_reliable_showdown_static():
+def test_modern_species_use_gen5_chain_even_when_outside_gen5_dex():
     urls = PokemonSpriteHelper.get_sprite("Armarouge", 936)
     assert isinstance(urls, list)
-    assert urls[0] == PokemonSpriteHelper.SHOWDOWN_STATIC.format(name="armarouge")
-    assert urls[-1] == PokemonSpriteHelper.OFFICIAL_ART.format(id="936")
+    assert urls[0].endswith("gen5/armarouge.png")
+    assert urls[1].endswith("gen5ani/armarouge.gif")
+    assert PokemonSpriteHelper.OFFICIAL_ART.format(id="936") not in urls
+
+
+def test_modern_species_without_fallback_use_static_gen5():
+    url = PokemonSpriteHelper.get_sprite(
+        "Baxcalibur", 998, style="animated", use_fallback=False
+    )
+    assert url.endswith("gen5/baxcalibur.png")
+
+
+def test_regional_forms_prefer_static_when_no_animation_available():
+    urls = PokemonSpriteHelper.get_sprite("growlithe-hisui", 58)
+    assert urls[0].endswith("gen5/growlithe-hisui.png")
+    assert urls[1].endswith("gen5ani/growlithe-hisui.gif")
 
 
 def test_gen_one_species_keep_gen5_animation_chain():
     urls = PokemonSpriteHelper.get_sprite("Charizard", 6)
     assert urls[0].endswith("gen5ani/charizard.gif")
     assert PokemonSpriteHelper.SHOWDOWN_STATIC.format(name="charizard") in urls
+
+
+def test_female_sprites_only_use_variant_when_available():
+    url = PokemonSpriteHelper.get_sprite("Frosmoth", 873, style="showdown", gender="female", use_fallback=False)
+    assert url.endswith("/frosmoth.png")
+
+
+def test_known_female_variant_keeps_gendered_slug():
+    url = PokemonSpriteHelper.get_sprite("Meowstic", 678, style="showdown", gender="female", use_fallback=False)
+    assert url.endswith("/meowstic-f.png")
